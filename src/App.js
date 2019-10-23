6ca0f5111099fdf9659360abb6c9d8d994a9e6f6
student <student@Admins-iMac.local>
2019-10-23 14:18:14 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {text: 'placeholder text from this.state.text',
+                    inputValue: '',
+                    loaded: false
+    };
+  }
+  hasLoaded = () => {
+    this.setState({loaded: !this.state.loaded})
+    console.log(this.state.loaded)
+  }
+  inputValue = (selectObject) =>{
+    this.setState({inputValue: selectObject.target.value })
+  } 
+
+  render(){
+    let h1;
+    if (this.state.loaded){
+        h1 = <h1>{this.props.string}<br></br> 
+          {this.state.text}<br></br>
+          {this.state.inputValue}
+          <br></br>
+          <input id = 'inputText' placeholder={this.state.text} onChange={this.inputValue.bind(this)}>
+          </input></h1>
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+        {h1}
+        {/* <h1>{this.props.string}<br></br> 
+        {this.state.text}<br></br>
+        {this.state.inputValue}
+        <input id = 'inputText' placeholder={this.state.text} onChange={this.inputValue.bind(this)}>
+        </input></h1> */}
+        <br></br>
+        <button onClick={this.hasLoaded} >Load Page</button>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
